15a4482f7d3b7f078cecba04b63c63e95798d4e0
officialksolomon <solomonuche42@gmail.com>
2022-11-07 12:29:35 +0100

    HNG backend task_2

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from api import views
 
 urlpatterns = [
     path('', view=views.AboutView.as_view(), name='about-home'),
-    path('about/', view=views.AboutView.as_view(), name='about')
+    path('about/', view=views.AboutView.as_view(), name='about'),
+    path('calculation/', view=views.CalculationView.as_view(), name='calculation'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import View
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import json
 # Create your views here.
 
 
@@ -12,3 +15,40 @@ class AboutView(View):
             "bio": "I am detailed oriented.,,"
         }
         return JsonResponse(about_info)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CalculationView(View):
+    def post(self, request):
+        if len(request.body) > 0 :
+            json_data = json.loads(request.body)
+            operation = json_data['operation_type']
+            print(request.body)
+            formatted_operation = operation.lower()
+            x = json_data['x']
+            y = json_data['y']
+            
+            if formatted_operation == 'multiplication':
+                result = {
+                "slackUsername": "solomonuche42",
+                "operation_type": formatted_operation,
+                "result": x * y,
+            }
+                return JsonResponse(result, safe=False)
+            elif formatted_operation =='addition':
+                result = {
+                "slackUsername": "solomonuche42",
+                "operation_type": formatted_operation,
+                "result": x + y,
+            }
+                return JsonResponse(result, safe=False)
+            elif formatted_operation == 'substraction':
+                result = {
+                "slackUsername": "solomonuche42",
+                "operation_type": formatted_operation,
+                "result": x - y,
+            }
+                return JsonResponse(result)
+            else:
+                return JsonResponse('Invalid operator', safe=False)
+        return JsonResponse('No data posted. Please post  JSON DAT.', safe=False)
